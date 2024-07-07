12584d6a1996aa2600d573933e5297ca688ca3a3
Nicolò Santamaria <nicolo.santamaria@protonmail.com>
2024-07-07 15:30:10 +0200

    Add DB for sharing

--- go.mod
+++ go.mod
@@ -1,9 +1,13 @@
 module tauplay
 
-go 1.21.6
+go 1.22.3
+
+toolchain go1.22.4
 
 require (
-	github.com/NicoNex/tau v1.6.4 // indirect
+	github.com/NicoNex/katalis v0.0.0-20240517170633-a61bdde47307 // indirect
+	github.com/NicoNex/tau v1.6.6 // indirect
+	github.com/akrylysov/pogreb v0.10.2 // indirect
 	golang.org/x/sys v0.0.0-20220730100132-1609e554cd39 // indirect
 	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035 // indirect
 )
--- main.go
+++ main.go
@@ -2,20 +2,92 @@ package main
 
 import (
 	"embed"
+	"flag"
 	"io/fs"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/NicoNex/katalis"
 )
 
-//go:embed assets
-var assets embed.FS
+type Page struct {
+	Code    string
+	Created time.Time
+	Updated time.Time
+}
 
-func main() {
+var (
+	//go:embed assets
+	assets embed.FS
+	db     katalis.DB[string, Page]
+)
+
+func handleGetCode(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func handleSetCode(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func retry(d time.Duration, fn func()) {
+	for {
+		fn()
+		time.Sleep(d)
+	}
+}
+
+func serve(port string) {
 	root, err := fs.Sub(assets, "assets")
 	if err != nil {
 		log.Fatal("fs.Sub", err)
 	}
 
 	http.Handle("/", http.FileServer(http.FS(root)))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("GET /p/{id}", handleGetCode)
+	http.HandleFunc("PUT /p/{id}", handleSetCode)
+
+	retry(time.Second*5, func() {
+		log.Println(http.ListenAndServe(port, nil))
+	})
+}
+
+func initDB() {
+	cache, err := os.UserCacheDir()
+	if err != nil {
+		log.Fatalln("initDB", "os.UserCacheDir", err)
+	}
+
+	db, err = katalis.Open(
+		filepath.Join(cache, "tauplay"),
+		katalis.StringCodec,
+		katalis.GobCodec[Page]{},
+	)
+	if err != nil {
+		log.Fatalln("initDB", "katalis.Open", err)
+	}
+}
+
+func main() {
+	port := *flag.String("p", ":8080", "The port TauPlay will listen to.")
+	flag.Parse()
+
+	if !strings.HasPrefix(port, ":") {
+		port = ":" + port
+	}
+
+	initDB()
+	go serve(port)
+
+	var signals = make(chan os.Signal, 1)
+
+	defer db.Close()
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGABRT)
+	<-signals
 }
