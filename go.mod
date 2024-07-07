8788a8469e9eed0060c2d4998d17eefe1275f6d3
Nicolò Santamaria <nicolo.santamaria@protonmail.com>
2024-07-07 18:25:19 +0200

    Finish implementing endpoints

--- go.mod
+++ go.mod
@@ -5,7 +5,11 @@ go 1.22.3
 toolchain go1.22.4
 
 require (
-	github.com/NicoNex/katalis v0.0.0-20240517170633-a61bdde47307 // indirect
+	github.com/NicoNex/katalis v0.0.0-20240517170633-a61bdde47307
+	github.com/google/uuid v1.6.0
+)
+
+require (
 	github.com/NicoNex/tau v1.6.6 // indirect
 	github.com/akrylysov/pogreb v0.10.2 // indirect
 	golang.org/x/sys v0.0.0-20220730100132-1609e554cd39 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"embed"
 	"flag"
+	"io"
 	"io/fs"
 	"log"
 	"net/http"
@@ -14,12 +15,13 @@ import (
 	"time"
 
 	"github.com/NicoNex/katalis"
+	"github.com/google/uuid"
 )
 
 type Page struct {
 	Code    string
 	Created time.Time
-	Updated time.Time
+	Read    time.Time
 }
 
 var (
@@ -29,11 +31,52 @@ var (
 )
 
 func handleGetCode(w http.ResponseWriter, r *http.Request) {
+	p, err := db.Get(r.PathValue("id"))
+	if err != nil {
+		log.Println("handleGetCode", "db.Get", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
+	go func(p Page) {
+		p.Read = time.Now()
+		if err := db.Put(r.PathValue("id"), p); err != nil {
+			log.Println("handleGetCode", "db.Put", err)
+			return
+		}
+	}(p)
+
+	if _, err := io.WriteString(w, p.Code); err != nil {
+		log.Println("handleGetCode", "io.WriteString", err)
+		return
+	}
 }
 
 func handleSetCode(w http.ResponseWriter, r *http.Request) {
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println("handleSetCode", "io.ReadAll", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
 
+	now := time.Now()
+	id := uuid.New().String()
+	err = db.Put(id, Page{
+		Code:    string(b),
+		Created: now,
+		Read:    now,
+	})
+	if err != nil {
+		log.Println("handleSetCode", "db.Put", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if _, err := io.WriteString(w, id); err != nil {
+		log.Println("handleSetCode", "io.WriteString", err)
+		return
+	}
 }
 
 func retry(d time.Duration, fn func()) {
@@ -50,8 +93,8 @@ func serve(port string) {
 	}
 
 	http.Handle("/", http.FileServer(http.FS(root)))
+	http.HandleFunc("PUT /p", handleSetCode)
 	http.HandleFunc("GET /p/{id}", handleGetCode)
-	http.HandleFunc("PUT /p/{id}", handleSetCode)
 
 	retry(time.Second*5, func() {
 		log.Println(http.ListenAndServe(port, nil))
@@ -84,10 +127,9 @@ func main() {
 
 	initDB()
 	go serve(port)
-
-	var signals = make(chan os.Signal, 1)
-
 	defer db.Close()
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGABRT)
-	<-signals
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGABRT)
+	<-sigs
 }
